f13ea5169276933159c704bceab7888ee5e2a9df
Zander <zander@zanderf.net>
2022-05-16 00:26:06 +0000

    mostly complete everything

--- README.md
+++ README.md
@@ -1,29 +1,74 @@
-<!--
+# omegga-currency
 
-When uploading your plugin to github/gitlab
-start your repo name with "omegga-"
+A lightweight currency plugin for Omegga.
 
-example: https://github.com/voximity/omegga-currency
-
-Your plugin will be installed via omegga install gh:voximity/currency
-
--->
-
-# currency
-
-A typed safe plugin for [omegga](https://github.com/brickadia-community/omegga).
-
-This is an auto-generated README file. Please change it before publishing your plugin!
-
-Be sure to edit the script in `setup.sh` to configure your plugin on install, or
-remove it entirely if it is unnecessary.
+**This is more of a dependency plugin than a feature plugin.**
+Other Omegga plugins can hook into this one and use it for its interop events.
+Omegga plugins that start with `omegga-currency-` are likely extension plugins and can interact
+directly with this one. Careful with which plugins you add, as they can harmfully impact your
+server's currency storage.
 
 ## Install
 
 `omegga install gh:voximity/currency`
 
-_TODO: more steps...?_
+Install some extension plugins!
 
 ## Usage
 
-_TODO: include some usage information_
+Configure the config options from the web panel and enjoy.
+
+## Making extensions
+
+This plugin makes use of Omegga's plugin interop system. The following events are available for you to use:
+
+| Event               | Args                                   | Description                                                                                                                                                                                    |
+| ------------------- | -------------------------------------- | ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- |
+| `get`               | [`player id: string`]                  | Gets the player's entire currency data object.                                                                                                                                                 |
+| `currency`          | [`player id: string`]                  | Gets the player's currency, formatted as a string, with the server's chosen prefix. **This is the preferred method of getting string formatted currency.**                                     |
+| `get.path.to.data`  | [`player id: string`]                  | Gets some data from the player's data object, given as a path. **This is the preferred method of getting the player's currency as a number.** Use `get.currency` to get the player's currency. |
+| `update`            | [`player id: string`, `data: object`]  | Updates the player's data object by merging it with the one given.                                                                                                                             |
+| `set.path.to.data`  | [`player id: string`, `data: any`]     | Sets a field in the player's data to the data given. If the path contains intermediate unset objects, they will be created as the data is traversed.                                           |
+| `add.path.to.data`  | [`player id: string`, `value: number`] | Assuming `path.to.data` represents a numeric field, adds `value` to it. If it is unset, it is set to `value`.                                                                                  |
+| `push.path.to.data` | [`player id: string`, `value: any`]    | Assuming `path.to.data` represents an array, pushes `value` onto the array. If the array is unset, it is set to `[value]`.                                                                     |
+| `delete.path.to.data` | [`player id: string`] | Deletes the field found at `path.to.data`, assuming it is not a field provided by this plugin, like `currency`. |
+
+### Errors
+
+If one of the events error, it will return an object of type `{ error: string }`.
+
+### Event object paths
+
+In the above, `path.to.data` represents a [jq](https://stedolan.github.io/jq/)-like path syntax that allows you to traverse down the player's data object. For example, assume the player's data object looks like this:
+
+```json
+{
+    "currency": 0,
+    "foo": {
+        "bar": 10
+    }
+}
+```
+
+If we use `add.foo.bar` with a value of `5`, the data object becomes:
+
+```json
+{
+    "currency": 0,
+    "foo": {
+        "bar": 15
+    }
+}
+```
+
+Or, we can use something like `push.foo.baz` with a value of `"hello"`. The data object then becomes:
+
+```json
+{
+    "currency": 0,
+    "foo": {
+        "bar": 15,
+        "baz": ["hello"]
+    }
+}
+```
--- omegga.plugin.ts
+++ omegga.plugin.ts
@@ -1,9 +1,9 @@
-import OmeggaPlugin, { OL, PS, PC, OmeggaPlayer } from 'omegga';
+import OmeggaPlugin, { OL, PS, PC } from 'omegga';
 
+type PlayerId = { id: string };
 type User = { currency: number };
-const DEFAULT_USER: User = { currency: 0 };
 
-type Config = { decimalPlaces: number };
+type Config = { decimalPlaces: number; prefix: string; baseCurrency: number };
 type Storage = { [cur_uuid: string]: User };
 
 export default class Plugin implements OmeggaPlugin<Config, Storage> {
@@ -11,42 +11,145 @@ export default class Plugin implements OmeggaPlugin<Config, Storage> {
   config: PC<Config>;
   store: PS<Storage>;
 
+  dependents: Set<string>;
+
   constructor(omegga: OL, config: PC<Config>, store: PS<Storage>) {
     this.omegga = omegga;
     this.config = config;
     this.store = store;
+    this.dependents = new Set();
   }
 
-  async getData({ id }: OmeggaPlayer): Promise<User> {
-    return (await this.store.get('cur_' + id)) ?? DEFAULT_USER;
+  defaultUser(): User {
+    return { currency: this.config.baseCurrency };
   }
 
-  async setData({ id }: OmeggaPlayer, data: User) {
+  roundCurrency(amount: number): number {
+    const pow = Math.pow(10, this.config.decimalPlaces ?? 0);
+    return Math.round(amount * pow) / pow;
+  }
+
+  navigatePath(data: any, path: string[]): any {
+    let nested = data;
+    while (path.length > 1) {
+      const [x] = path.splice(0, 1);
+      nested = nested[x] ??= {};
+    }
+    return [nested, path[0]];
+  }
+
+  formatCurrency(amount: number): string {
+    return (
+      this.config.prefix +
+      this.roundCurrency(amount).toFixed(this.config.decimalPlaces)
+    );
+  }
+
+  async getData({ id }: PlayerId): Promise<User> {
+    return (await this.store.get('cur_' + id)) ?? this.defaultUser();
+  }
+
+  async setData({ id }: PlayerId, data: User) {
+    if ('currency' in data) data.currency = this.roundCurrency(data.currency);
     await this.store.set('cur_' + id, data);
   }
 
-  async updateData({ id }: OmeggaPlayer, data: Partial<User>) {
-    const baseData = (await this.store.get('cur_' + id)) ?? DEFAULT_USER;
+  async updateData({ id }: PlayerId, data: Partial<User>) {
+    if ('currency' in data) data.currency = this.roundCurrency(data.currency);
+    const baseData = (await this.store.get('cur_' + id)) ?? this.defaultUser();
     await this.store.set('cur_' + id, { ...baseData, ...data });
   }
 
-  async updateDataDelta({ id }: OmeggaPlayer, transform: (base: User) => User) {
-    const baseData = (await this.store.get('cur_' + id)) ?? DEFAULT_USER;
-    await this.store.set('cur_' + id, { ...baseData, ...transform(baseData) });
+  async getCurrency(pid: PlayerId): Promise<string> {
+    const data = await this.getData(pid);
+    return this.formatCurrency(data.currency ?? 0);
   }
 
   async init() {
-    // Write your plugin!
-    this.omegga.on('cmd:test', (speaker: string) => {
-      this.omegga.broadcast(`Hello, ${speaker}!`);
+    this.omegga.on('cmd:currency', async (speaker: string) => {
+      this.omegga.whisper(
+        speaker,
+        `You currently have <color="ff0">${await this.getCurrency(
+          this.omegga.getPlayer(speaker)
+        )}</>.`
+      );
     });
 
-    return { registeredCommands: ['test'] };
+    return { registeredCommands: ['currency'] };
   }
 
-  async pluginEvent(event: string, from: string, ...args: any[]) {}
+  async pluginEvent(event: string, from: string, args: any[]) {
+    if (!this.dependents.has(from)) {
+      this.dependents.add(from);
+      console.log(`Plugin "${from}" is interacting with the currency plugin`);
+    }
 
-  async stop() {
-    // Anything that needs to be cleaned up...
+    if (event === 'get') {
+      return await this.getData({ id: args[0] });
+    } else if (event === 'currency') {
+      return await this.getCurrency({ id: args[0] });
+    } else if (event.startsWith('get.')) {
+      const data = await this.getData({ id: args[0] });
+      const [nested, field] = this.navigatePath(
+        data,
+        event.substring(4).split('.')
+      );
+      return nested[field];
+    } else if (event === 'update') {
+      await this.updateData({ id: args[0] }, args[1]);
+    } else if (event.startsWith('set.')) {
+      const data = await this.getData({ id: args[0] });
+      const [nested, field] = this.navigatePath(
+        data,
+        event.substring(4).split('.')
+      );
+      nested[field] = args[1];
+      await this.updateData({ id: args[0] }, data);
+    } else if (event.startsWith('add.')) {
+      const data = await this.getData({ id: args[0] });
+      const [nested, field] = this.navigatePath(
+        data,
+        event.substring(4).split('.')
+      );
+      if (nested[field] == null || typeof nested[field] === 'number') {
+        if (typeof args[1] !== 'number')
+          return { error: 'Must add a number to a number field' };
+        nested[field] = (nested[field] ?? 0) + args[1];
+      } else {
+        return { error: 'Cannot add to a field that is not a number' };
+      }
+      await this.updateData({ id: args[0] }, data);
+    } else if (event.startsWith('push.')) {
+      const data = await this.getData({ id: args[0] });
+      const [nested, field] = this.navigatePath(
+        data,
+        event.substring(5).split('.')
+      );
+      if (nested[field] == null || Array.isArray(nested[field])) {
+        (nested[field] ??= []).push(args[1]);
+      } else {
+        return { error: 'Cannot add to a field that is not a number' };
+      }
+      await this.updateData({ id: args[0] }, data);
+    } else if (event.startsWith('delete.')) {
+      const data = await this.getData({ id: args[0] });
+      const [nested, field] = this.navigatePath(
+        data,
+        event.substring(5).split('.')
+      );
+      if (nested === data && field in this.defaultUser()) {
+        return { error: 'Cannot delete a base field' };
+      }
+      delete nested[field];
+      await this.setData({ id: args[0] }, data);
+    } else if (event.startsWith('format')) {
+      if (typeof args[0] !== 'number')
+        return { error: 'Argument to `format` must be a number' };
+      return this.formatCurrency(Number(args[0]));
+    } else {
+      return { error: 'Invalid event ' + event };
+    }
   }
+
+  async stop() {}
 }
